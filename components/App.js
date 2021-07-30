9b085ffd739d430ca3ad0a3f976992cf9acf0170
Michelle <michelle.thompson@att.net>
2021-07-29 20:57:44 -0400

    final submission

---
+++ components/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+import Header from './Header/Header';
+import Compose from './Compose/Compose';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      posts: []
+    };
+
+    this.updatePost = this.updatePost.bind( this );
+    this.deletePost = this.deletePost.bind( this );
+    this.createPost = this.createPost.bind( this );
+  }
+  
+  componentDidMount() {
+    axios.get('https://practiceapi.devmountain.com/api/posts')
+    .then(response => {
+      this.setState({
+        posts: response.data
+      })
+    }).catch(err=> {
+      console.log(err)
+    })
+  }
+
+  updatePost() {
+  
+  }
+
+  deletePost() {
+
+  }
+
+  createPost() {
+
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div className="App__parent">
+        <Header />
+
+        <section className="App__content">
+
+          <Compose />
+          
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
